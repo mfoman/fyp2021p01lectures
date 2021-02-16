190521ecc33eb50d99d4a05cd61bbe957b1747bc
DanielRingHansen <danielring320@gmail.com>
2021-02-16 18:16:27 +0100

    Small changes/Cleaning up

--- src/scripts/process_bradford_pd.py
+++ src/scripts/process_bradford_pd.py
@@ -10,49 +10,39 @@ PATH = {
     'interim': Path('./data/interim/'),
 }
 
-PATH['accident'] = PATH['raw'] / "Road Safety Data - Accidents 2019.csv"
-PATH['casual'] = PATH['raw'] / "Road Safety Data - Casualties 2019.csv"
+PATH['accidents'] = PATH['raw'] / "Road Safety Data - Accidents 2019.csv"
+PATH['casualties'] = PATH['raw'] / "Road Safety Data - Casualties 2019.csv"
 PATH['vehicles'] = PATH['raw'] / "Road Safety Data - Vehicles 2019.csv"
 
 
 # === Read the data
 DATA = {}
 
-DATA['accident'] = pd.read_csv(PATH['accident'], dtype={
-                               0: 'string', 31: 'string'}, encoding='utf-8-sig')
+DATA['accidents'] = pd.read_csv(PATH['accidents'], dtype={0: 'string', 31: 'string'}, encoding='utf-8-sig')
 
-DATA['casual'] = pd.read_csv(PATH['casual'], dtype={
-                             0: 'string'}, encoding='utf-8-sig')
+DATA['casualties'] = pd.read_csv(PATH['casualties'], dtype={0: 'string'}, encoding='utf-8-sig')
 
-DATA['vehicles'] = pd.read_csv(PATH['vehicles'], dtype={
-                               0: 'string'}, encoding='utf-8-sig')
+DATA['vehicles'] = pd.read_csv(PATH['vehicles'], dtype={0: 'string'}, encoding='utf-8-sig')
 
 
 # === Process it
 # Get all bradford accidents as we know they're in local district 200
-DATA['brad_accident'] = DATA['accident'][DATA['accident']
-                                         ['Local_Authority_(District)'] == 200]
+DATA['brad_accidents'] = DATA['accidents'][DATA['accidents']['Local_Authority_(District)'] == 200]
+
 
 # Get the accident indexes from brad_accident
-accident_index = DATA['brad_accident']['Accident_Index']
+accident_index = DATA['brad_accidents']['Accident_Index']
+
 
 # Use the index to find all matching rows in casualties and vehicles
-brad_cass_mask = DATA['casual']['Accident_Index'].isin(accident_index)
+brad_cass_mask = DATA['casualties']['Accident_Index'].isin(accident_index)
 brad_vehi_mask = DATA['vehicles']['Accident_Index'].isin(accident_index)
 
-DATA['brad_casual'] = DATA['casual'][brad_cass_mask]
+DATA['brad_casualties'] = DATA['casualties'][brad_cass_mask]
 DATA['brad_vehicles'] = DATA['vehicles'][brad_vehi_mask]
 
 
 # === Save it
 DATA['brad_accident'].to_csv(PATH['interim'] / 'bradford_accidents.csv')
-DATA['brad_casual'].to_csv(PATH['interim'] / 'bradford_casualties.csv')
+DATA['brad_casualties'].to_csv(PATH['interim'] / 'bradford_casualties.csv')
 DATA['brad_vehicles'].to_csv(PATH['interim'] / 'bradford_vehicles.csv')
-
-
-# === Old code
-# tmp_inner = pd.merge(DATA['acc_bradford'], DATA['casual'], how="inner", on=[
-#     "Accident_Index", "Accident_Index"])
-
-# tmp_inner = pd.merge(tmp_inner, DATA['vehicles'], how="inner", on=[
-#     "Accident_Index", "Accident_Index"])
